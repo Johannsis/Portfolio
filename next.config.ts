19568a79a60d66a8455393fd776e3d21727e7fd7
Johannes Hoersch <johanneshoersch@gmail.com>
2024-11-09 13:06:28 -0400

    test

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  assetPrefix: isProd ? "/portfolio/" : "",
-  basePath: isProd ? "/portfolio" : "",
-  output: "export",
+  assetPrefix: isProd ? '/portfolio/' : '',
+  basePath: isProd ? '/portfolio' : '',
+  output: 'export',
   reactStrictMode: true,
   images: {
     unoptimized: true,
