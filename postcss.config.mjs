55ac5163fe1a82b28d9a32305e96d3f94818c01c
Johannes Hoersch <johanneshoersch@gmail.com>
2024-11-13 20:28:39 -0400

    test

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,8 +2,10 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
+    'postcss-import': {},
     tailwindcss: {},
     autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
   },
 };
 
