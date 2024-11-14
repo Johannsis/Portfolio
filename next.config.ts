1783489a1baccee5e01cb97129666edfc37ff02c
Johannes Hoersch <johanneshoersch@gmail.com>
2024-11-13 20:20:35 -0400

    test

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
+  assetPrefix: isProd ? '/out/' : '',
+  basePath: isProd ? '/out' : '',
   output: 'export',
   reactStrictMode: true,
   images: {
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,10 +2,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    'postcss-import': {},
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
   },
 };
 
